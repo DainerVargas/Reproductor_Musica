fa880759240b67aea937d1577fa1bf38a59a3e22
DainerVargas <dainer2607@.com>
2023-11-21 14:09:13 -0500

    Mejoras en el reproductor

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Reproductor de musica</h1>
+<div align="center"><img src="images/Reproductor musica.png" alt=""></div>
--- html/main.js
+++ html/main.js
@@ -1,39 +1,49 @@
-var audio = document.getElementById('audio-player');
-var playBtn = document.getElementById('play');
-var pauseBtn = document.getElementById('pause');
-var prevBtn = document.getElementById('prev');
-var nextBtn = document.getElementById('next');
-var imagen = document.getElementById('imagen');
-var volumeSlider = document.getElementById('volume-slider');
+const audio = document.getElementById('audio-player');
+const playBtn = document.getElementById('play');
+const pauseBtn = document.getElementById('pause');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const imagen = document.getElementById('imagen');
+const volumeSlider = document.getElementById('volume-slider');
 
-var image=[
+const image=[
   '../images/310 - la banda del 5.jpg',
   '../images/la ocasion.jpg',
   '../images/me niegas.jpg',
   '../images/diego daza - que vuelva.jpg',
 ]
 
-var imgNext=0;
+let imgNext=0;
 
-var songs = [ 
+const songs = [ 
   '../audio/310 - La Banda Del 5 Feat. Diego Daza(MP3_128K).mp3',
   '../audio/La Ocasión - De La Ghetto_ Arcangel_ Ozuna_ Anuel Aa_ Dj Luian_ Mambo Kingz [Video Oficial](MP3_70K).mp3',
   '../audio/Me Niegas - Baby Rasta y Gringo (Letra)(MP3_128K).mp3',
   '../audio/Que Vuelva - Diego Daza _ Carlos Rueda(MP3_128K).mp3',
 ];
 
-var SongIndex = 0;
+let SongIndex = 0;
 
+const nombreSong = [
+  '310-La Banda Del 5','La Ocasión','Me Niegas','Que Vuelva'
+];
+
+let nombreIndex = 0;
+
+const titulo = document.getElementById('nombre');
 // Función para pasar a la siguiente canción
 function nextSong() {
   SongIndex++;
   imgNext++;
+  nombreIndex++;
   if (SongIndex >= songs.length) {
     imgNext = 0;
-    SongIndex = 0; // Vuelve a la primera canción si llega al final
+    SongIndex = 0;
+    nombreIndex = 0;
   }
   audio.src = songs[SongIndex];
   imagen.src = image[imgNext];
+  titulo.innerHTML = nombreSong[nombreIndex];
   audio.play();
 }
 
@@ -41,16 +51,19 @@ function nextSong() {
 function prevSong() {
   SongIndex--;
   imgNext--;
+  nombreIndex--;
   if (SongIndex < 0) {
     imgNext = image.length - 1;
-    SongIndex = songs.length - 1; // Retrocede a la última canción si está en la primera
+    SongIndex = songs.length - 1;
+    nombreIndex = nombreSong.length - 1;
   }
   audio.src = songs[SongIndex];
   imagen.src = image[imgNext]
+  titulo.innerHTML = nombreSong[nombreIndex];
   audio.play();
 }
 volumeSlider.addEventListener('input', function() {
-    var volumeValue = parseFloat(volumeSlider.value);
+    const volumeValue = parseFloat(volumeSlider.value);
     audio.volume = volumeValue;
 });
 
